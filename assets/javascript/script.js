1d62879b61b5cbc2c7d5a8c8f1e61b237a11cf16
Valerii Bihun <valeriibihun.co@gmail.com>
2021-09-21 20:48:43 -0600

    added a global object with questions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,19 +4,53 @@ var startButton = document.querySelector
 var section = document.querySelector("#section");
 var footer = document.querySelector("footer");
 
+// setting a variable with questions and answers
+var questions = [
+    firstQuestion = {
+        question : "What is JavaScript?",
+        answer : [ "A currency;", "A browser;", "A football team;","A scripting language." ],
+        correctAnswer : 3
+    },
+
+    secondQuestion = {
+        question : "What are three ways to declare a variable in JavaScript?",
+        answer : [ "var, let , const;", " if, else, for;", "var, iab, le;","var(), var {}, var <>." ],
+        correctAnswer : 0
+    },
+
+    thirdQuestion = {
+        question : "A JavaScript string is normally written inside what symbols?",
+        answer : [ "Quotes;", "Brackets;", "Parenthesis;","Dots." ],
+        correctAnswer : 0
+    },
+
+    fourthQuestion = {
+        question : "Can you store multiple values in JavaScript arrays?",
+        answer : [ "Yes;", "No."],
+        correctAnswer : 0
+    },
+
+    fifthQuestion = {
+        question : "What is not a condition statement in JavaScript?",
+        answer : [ "If;", "Who;", "Else;","Else if;"],
+        correctAnswer : 1
+    },
+]
+
 // Global var to keep track of score
 var score = 0;
 
 // Global var for timer seconds
 var timer = 90;
 
-// Timer function
+// Main function that triggers displaying questions\hiding greeting text and starts timer
 function main() {
     display();
     startTimer();
     
 }
 
+// Timer function
 function startTimer() {
     var timerCount = setInterval(function () {
         timer--;
@@ -34,7 +68,7 @@ function startTimer() {
     }, 1000);
   }
 
-//   Hides main text after start button is clicked and starts displaying questions
+//   Hides main text after start button is clicked and starts displaying questions and footer
 function display(){
     section.setAttribute("style", "display:none;");
     footer.setAttribute("style", "display:block;");
