218206bd9c97f90b7c1002dcf883623e19b9f273
Valerii Bihun <valeriibihun.co@gmail.com>
2021-09-22 09:47:17 -0600

    added an ability to save a name for a score

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,11 +8,15 @@ var questionEl = document.querySelector("#questionText");
 var answerEl = document.querySelector("#answer");
 var promptEl = document.querySelector("#prompt");
 var form = document.querySelector("#form");
-var username = document.querySelector("#username");
 var submit = document.querySelector("#submitButton");
+var scoreValue = [];
+// var scoreName = document.querySelector("#username");
+var username = "";
+var highScores = [];
 
-// Global var to keep track of score
-var score = 0;
+
+
+
 
 // Global var for timer seconds
 var timer = 90;
@@ -59,6 +63,8 @@ function startTimer() {
         timer--;
       timerElement.textContent = timer;
       if (timer === 0 || timer <= 0) {
+        //   making sure that points dont go to negative numbers
+        timer = 0;
         clearInterval(timerCount);
         results();
       }
@@ -84,11 +90,13 @@ function answersButtons(e){
         if (currentQuestion === questions.length-1){
             results();
         }
+        else {
         var index = parseInt(e.target.parentElement.dataIndex);
         checkAnswer(index);
         removeChildren(answerEl);
         currentQuestion++;
-        showQuestion();        
+        showQuestion();   
+    }     
     }
 }
 
@@ -96,12 +104,11 @@ function answersButtons(e){
 function checkAnswer(index){
     if(index === questions[currentQuestion].correctAnswer){
         promptEl.style.opacity = 1;
-        promptEl.textContent = "Corret!";
-        score++; 
+        promptEl.textContent = "You are correct!";
     }
     else{
         promptEl.style.opacity = 1;
-        promptEl.textContent = "Wrong!";
+        promptEl.textContent = "You are wrong!";
         timer = timer - 30;
         
     }
@@ -117,36 +124,68 @@ function removeChildren(parent){
 
 //shows users score on the page
 function results(){
-    questionSection.textContent = "Your Score: " + timer;
+    if (timer >= 2){
+    questionSection.textContent = "You passed! Your Score: " + timer + " points.";
+    
+    }
+    if ( timer == 1) {
+    questionSection.textContent = "You passed! Your Score: " + timer + " point.";
+    var scoreValue = {
+        name: username,
+        score: timer,
+      };
+    // localStorage.setItem("scoreValue", JSON.stringify(scoreValue));
+    } 
+    if (timer == 0 ){
+    questionSection.textContent = "You failed! Your Score: " + timer + " points.";
+    }
+    username = prompt("Enter your username!");
+    alert("Thank you, your score is saved!");
+    var scoreValue = {
+        name: username,
+        score: timer,
+      };
+    localStorage.setItem("scoreValue", JSON.stringify(scoreValue));
     timerElement.setAttribute("style", "display:none;");
     form.setAttribute("style", "display:block;");
+
     
 }
 
-submit.addEventListener("click", saveScore);
+// submit.addEventListener("click", function (event){
+//     event.preventDefault;
+//     saveScore();
+// });
+    
+
+// submit.addEventListener("click", saveScore);
   
 
-function saveScore() {
-    var scoreValue = {
-      name: username.value,
-      score: score,
-    };
-    localStorage.setItem("scoreValue", JSON.stringify(scoreValue));
-  }
+// function saveScore() { 
+//     var scoreValue = {
+//       name: username.value,
+//       score: score,
+//     };
+//     localStorage.setItem("scoreValue", JSON.stringify(scoreValue));
+//   }
 
 // Main function that triggers displaying questions\hiding greeting text and starts timer
 function main() {
+    // username = prompt("Enter your username!");
+    // alert("Thank you and good luck!");
     display();
     startTimer();
     showQuestion();
     
 }
 
+
 //   Hides main text after start button is clicked and starts displaying questions and footer
 function display(){
     section.setAttribute("style", "display:none;");
     footer.setAttribute("style", "display:block;");
   }
+
 
 // event listener to call the function for answer check
 answerEl.addEventListener("click",answersButtons);
